bfcf90e0d03d84bcfb0682ba12672fb22bbfeac2
Gunturadhtya <gunturpdng@gmail.com>
2025-10-07 16:46:02 +0800

    Update Soal3Main.java

--- Pertemuan 2/PRAK203_2410817310003_MUHAMMAD_GUNTUR_RICKY_ADHITYA/Soal3Main.java
+++ Pertemuan 2/PRAK203_2410817310003_MUHAMMAD_GUNTUR_RICKY_ADHITYA/Soal3Main.java
@@ -4,7 +4,7 @@ public class Soal3Main {
 	public static void main(String[] args) {
 			Pegawai p1 = new Pegawai();
 
-//			baris ini error karena tidak memiliki tanda koma di akhir baris
+//			baris ini error karena tidak memiliki titik koma di akhir baris
 //			p1.nama = "Roi"
 			p1.nama = "Roi";
 			p1.asal = "Kingdom of Orvel";
